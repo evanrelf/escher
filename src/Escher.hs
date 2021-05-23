a239c53df579393447a6bd42ddd8259647c9d01e
Evan Relf <evan@evanrelf.com>
2021-05-23 10:42:42 -0700

    Receive handshake and status request packets

--- src/Escher.hs
+++ src/Escher.hs
@@ -2,21 +2,59 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeApplications #-}
+
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module Escher where
 
+import Data.ByteString (ByteString)
+import Escher.Packets (Handshake, StatusRequest)
 import Network.Run.TCP (runTCPServer)
 
-import qualified Data.ByteString as ByteString
+import qualified Data.Serialize as Cereal
+import qualified Network.Socket as Network
 import qualified Network.Socket.ByteString as Network
-import qualified System.IO as IO
 
 main :: IO ()
 main = do
   putStrLn "Listening on port 3000"
   runTCPServer (Just "127.0.0.1") "3000" \socket -> do
-    let loop = do
-          bytes <- Network.recv socket 1024
-          ByteString.hPut IO.stdout bytes
-          loop
-    loop
+    (result, rest) <- receive @Handshake socket Nothing
+
+    case result of
+      Left err ->
+        putStrLn ("!!! Failed to parse handshake:\n" <> err)
+
+      Right handshake -> do
+        putStrLn "%%% Successful parsed handshake:"
+        print handshake
+        putStr "\n\n"
+
+    (result, _rest) <- receive @StatusRequest socket (Just rest)
+
+    case result of
+      Left err ->
+        putStrLn ("!!! Failed to parse status request:\n" <> err)
+
+      Right statusRequest -> do
+        putStrLn "%%% Successful parsed status request:"
+        print statusRequest
+        putStr "\n\n"
+
+receive
+  :: Cereal.Serialize a
+  => Network.Socket
+  -> Maybe ByteString
+  -> IO (Either String a, ByteString)
+receive socket = go (Cereal.runGetPartial Cereal.get)
+  where
+    go k start = do
+      bytes <- case start of
+        Just bytes -> pure bytes
+        Nothing -> Network.recv socket 1024
+
+      case k bytes of
+        Cereal.Fail err rest -> pure (Left err, rest)
+        Cereal.Done x rest -> pure (Right x, rest)
+        Cereal.Partial k' -> go k' Nothing
