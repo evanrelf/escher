a1774f2d25049baf175a1703c0baa5d17c6b4587
Evan Relf <evan@evanrelf.com>
2021-05-23 13:32:07 -0700

    Start working on test suite

---
+++ test/Main.hs
@@ -0,0 +1,22 @@
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
+module Main (main) where
+
+import Text.Show.Pretty (ppShow)
+
+import qualified Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Test.Tasty as Tasty
+import qualified Test.Tasty.Golden as Tasty.Golden
+import qualified Test.Tasty.HUnit as Tasty.HUnit
+import qualified Test.Tasty.Hedgehog as Tasty.Hedgehog
+
+main :: IO ()
+main = Tasty.defaultMain $ Tasty.testGroup "tests"
+  [ test_foo
+  ]
+
+test_foo :: Tasty.TestTree
+test_foo = Tasty.testGroup "foo"
+  [
+  ]
